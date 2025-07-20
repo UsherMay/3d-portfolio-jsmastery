88a272afc3f5754483c60856761398e74585f3ad
UsherMay <98615933+UsherMay@users.noreply.github.com>
2025-07-20 11:43:27 +0200

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar";
 import Contact from "./sections/Contact";
 import ExperienceSection from "./sections/ExperienceSection";
 import FeatureCards from "./sections/FeatureCards";
+import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
 import LogoSection from "./sections/LogoSection";
 import ShowcaseSection from "./sections/ShowcaseSection";
@@ -20,6 +21,7 @@ function App (){
       <TechStack/>
       <Testimonials/>
       <Contact/>
+      <Footer/>
     </>
     // <main>
     //   <div>
---
+++ src/sections/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { socialImgs } from '../constants'
+
+function Footer () {
+    return (
+        <footer className='footer'>
+            <div className='footer-container'>
+                <div className=' flex flex-col justify-center md:items-start items-center'>
+                    <a href='/' >
+                        Visit my blog
+                    </a>
+                </div>
+                <div className='socials'>
+                    {socialImgs.map((social) => (
+                        <a key={social.name} className='icon' target='_blank' href={social.url}>
+                            <img src={social.imgPath} alt={social.name} />
+                        </a>
+                    ))}
+                </div>
+
+                <div className='flex flex-col justify-center'>
+                    <p className='text-center md:text-end'>
+                        &copy; {new Date().getFullYear()} Adrian | JS Mastery. All rights reserved.
+                    </p>    
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
