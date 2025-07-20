8afdb75cf66fee74edbad72196ca5e886f4e6e26
UsherMay <98615933+UsherMay@users.noreply.github.com>
2025-07-20 10:49:44 +0200

    Testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./sections/Hero";
 import LogoSection from "./sections/LogoSection";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import TechStack from "./sections/TechStack";
+import Testimonials from "./sections/Testimonials";
 
 function App (){
   return (
@@ -16,6 +17,7 @@ function App (){
       <FeatureCards/>
       <ExperienceSection/>
       <TechStack/>
+      <Testimonials/>
     </>
     // <main>
     //   <div>
